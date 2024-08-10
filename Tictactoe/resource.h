d45874c6872b454a73b2eb8f07666697b75c4df2
jlu005807 <1289406660@qq.com>
2024-08-10 22:10:14 +0800

    Modifying Absolute Paths to Relative Paths for Packaging into EXE

---
+++ Tictactoe/resource.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 Resource.rc 使用
+//
+#define IDB_PNG1                        102
+#define backgroud                       102
+#define piece_X                         103
+#define IDR_IMAGE2                      104
+#define piece_O                         104
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
